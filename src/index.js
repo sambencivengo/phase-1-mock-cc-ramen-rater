e5148f840f08f1fa690e611d00489dfb937151eb
Sam Bencivengo <sambencivengo@gmail.com>
2021-09-29 11:53:26 -0400

    hmmm

--- src/index.js
+++ src/index.js
@@ -9,18 +9,19 @@ function start() {
 function getRamen() {
   fetch(mainURL)
     .then((response) => response.json())
-    .then((ramenData) => ramenData.forEach((ramen) => renderRamen(ramen)));
+    .then((ramenData) => ramenData.forEach(renderRamen));
 }
 
 //return a usable object that we can iterate
 
 //grab div where the images will be stored
 const ramenMenu = document.getElementById('ramen-menu');
-function renderRamen() {
-  let ramenPic = document.createElement('img');
-  ramenPic.src = `${getRamen}`;
-  ramenPic.className = 'ramen-image';
-  ramenMenu.appendChild(ramenPic);
+function renderRamen(ramen) {
+  // let ramenPic = document.createElement('img');
+  // ramenPic.src = ramen.image;
+  // ramenPic.className = 'ramen-image';
+  // ramenMenu.appendChild(ramenPic);
+  console.log(ramen);
 }
 
 renderRamen();
